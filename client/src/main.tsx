a7e1e931a3e6e197f65c8a7ed722943d40791044
Jakkiinofglobal <whenefhuttontalks@gmail.com>
2025-10-18 00:13:23 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/routes";
+import { AuthProvider } from "@/context/auth";
+import { UserPrefsProvider } from "@/context/userPrefs";
+import { PlanProvider } from "@/context/plan";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <UserPrefsProvider>
+      <PlanProvider>
+        <RouterProvider router={router} />
+      </PlanProvider>
+    </UserPrefsProvider>
+  </AuthProvider>
+);
